265bc9e6622ff85d20f92fdd31617b6c9cf05d3a
desko27 <desko27@gmail.com>
2021-02-16 18:20:30 +0100

    Add Portuguese by @joaochris

--- app/src/translations/index.js
+++ app/src/translations/index.js
@@ -1,7 +1,9 @@
 import en from './en'
 import es from './es'
+import pt from './pt'
 
 export default {
   en,
-  es
+  es,
+  pt
 }
---
+++ app/src/translations/pt/index.js
@@ -0,0 +1,9 @@
+import lobby from './lobby.json'
+import waiting from './waiting.json'
+import app from './app.json'
+
+export default {
+  lobby,
+  waiting,
+  app
+}
